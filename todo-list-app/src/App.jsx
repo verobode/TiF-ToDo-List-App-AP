d47ae9ef64dfc9a361163e585ce6d792e222a687
ViktorPls <gabriel.vides96@gmail.com>
2023-11-23 14:30:04 +0100

    Refactor todo list app UI using Chakra UI

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Box, ChakraProvider, Flex, Button, Spacer, ButtonGroup, Checkbox, Text } from '@chakra-ui/react'
+import { Center, Heading } from '@chakra-ui/react'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChakraProvider>
+      <Box >
+        <Heading mb={12}>TP F- Grupo J</Heading>
+
+        <Box>
+          {/* Todo - Header */}
+          <Flex minWidth='max-content' alignItems='center' gap='2' m={8} p={4} mb={0} bg={'gray'}>
+            <Box p='2'>
+              <Heading size='md' color={'white'}>Chakra App</Heading>
+            </Box>
+            <Spacer />
+            <ButtonGroup gap='2'>
+              <Button colorScheme='teal'>+</Button>
+            </ButtonGroup>
+          </Flex>
+          {/* Todo - Body */}
+          <Flex border='1px' borderColor='gray.200' mx={8} p={4}>
+            <Checkbox colorScheme='green' py={2} px={4} defaultChecked></Checkbox>
+            <Center>
+              <Text fontSize='lg'>Throw away my books</Text>
+            </Center>
+          </Flex>
+
+          <Flex border='1px' borderColor='gray.200' mx={8} p={4}>
+            <Checkbox colorScheme='green' py={2} px={4}></Checkbox>
+            <Text fontSize='lg'>Love with React</Text>
+          </Flex>
+
+          <Flex border='1px' borderColor='gray.200' mx={8} p={4}>
+            <Checkbox colorScheme='green' py={2} px={4} ></Checkbox>
+            <Text fontSize='lg'>War against Chakra UI</Text>
+          </Flex>
+        </Box>
+      </Box>
+    </ChakraProvider>
   )
 }
 
